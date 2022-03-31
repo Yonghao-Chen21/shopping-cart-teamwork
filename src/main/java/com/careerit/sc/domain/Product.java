77944d4d77ee52ec523dd1a65652e4ced7b21394
madhu1105 <80799049+madhu1105@users.noreply.github.com>
2022-03-31 13:46:06 -0400

    Update Product.java

--- src/main/java/com/careerit/sc/domain/Product.java
+++ src/main/java/com/careerit/sc/domain/Product.java
@@ -1,5 +1,21 @@
 package com.careerit.sc.domain;
 
+import java.util.*;
+import lombok.Data;
+import lombok.Builder;
+
+@Data
+@Builder
+
+
 public class Product {
+	
+	private int Product_ID;
+	private String Product_Name;
+	private String Type;
+	private String Description;
+	private float Price;
+	private int In_stock;
 
 }
+
