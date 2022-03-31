2f52febe493fa7cd5f5776273d8c56ebb6ebb726
madhu1105 <80799049+madhu1105@users.noreply.github.com>
2022-03-31 13:45:35 -0400

    Update User.java

--- src/main/java/com/careerit/sc/domain/User.java
+++ src/main/java/com/careerit/sc/domain/User.java
@@ -1,5 +1,18 @@
 package com.careerit.sc.domain;
+import java.util.*;
+import lombok.Data;
+import lombok.Builder;
+
+@Data
+@Builder
+
+
 
 public class User {
-
+	
+	
+	private String Username;
+	private String password;
+	private String email;
+	
 }
